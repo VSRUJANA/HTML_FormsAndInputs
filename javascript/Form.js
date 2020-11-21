212ed0a3309c30311e563e8240a78f5b16ebd6e1
VSRUJANA <srujana.valavala@gmail.com>
2020-11-22 01:15:57 +0530

    [srujana] Add. Validate Name entered by user

--- javascript/Form.js
+++ javascript/Form.js
@@ -1,14 +1,3 @@
-// Event listener for Name
-const text = document.querySelector('#text');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function () {
-    let namRegex = new RegExp(/^[A-Z][a-z]{2,}$/);
-    if (namRegex.test(text.value))
-        textError.textContent = "";
-    else
-        textError.textContent = "Invalid Name";
-})
-
 // Event listener for salary range
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
@@ -16,3 +5,15 @@ output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 })
+
+// Validate Name
+const name = document.querySelector('#name');
+const error = document.querySelector('.text-error');
+name.addEventListener('input', function () 
+{
+    let nameRegex = new RegExp(/^[A-Z][a-z]{2,}$/);
+    if (nameRegex.test(name.value))
+        error.textContent = "";
+    else
+        error.textContent = "Invalid Name";
+});
